171d1cc07845c3afbb335bad1756ed39f791ed5f
anantariskys <gamersindo555@gmail.com>
2024-12-31 02:57:29 +0700

    feat(seeder) : add driver

---
+++ database/seeders/DriverSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Driver;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DriverSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $drivers = [
+            [
+                'name' => 'Joko Susilo',
+                'status' => 'assigned',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Budi Rahayu',
+                'status' => 'available',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Agus Salim',
+                'status' => 'assigned',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Jaya Kusuma',
+                'status' => 'available',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Budi Santoso',
+                'status' => 'available',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
+
+        Driver::insert($drivers);
+    }
+}
