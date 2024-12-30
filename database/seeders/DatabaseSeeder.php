6b4aeedd0254ea1ab0dca172dd9e8bd941d70ba0
anantariskys <gamersindo555@gmail.com>
2024-12-31 02:57:11 +0700

    feat : calling split seeder on main seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Vehicle;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+       $this->call(
+        [
+            DriverSeeder::class,
+            VehicleSeeder::class,
+            UserSeeder::class
+        ]
+       );
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+
+        
     }
+
 }
