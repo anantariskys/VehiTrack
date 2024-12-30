6ee1363915763bdc5309f2f3af3ae9d819cf6c19
anantariskys <gamersindo555@gmail.com>
2024-12-31 03:00:06 +0700

    refactor : simplify the structure

--- resources/js/Components/Sidebar.tsx
+++ resources/js/Components/Sidebar.tsx
@@ -3,34 +3,60 @@ import { Link } from "@inertiajs/react";
 
 const Sidebar = () => {
     return (
-        <div className="max-w-72 bg-primary gap-4 text-tertiary flex flex-col px-4 py-8 justify-start items-center shadow-lg h-screen w-full">
+        <div className="max-w-72 sticky top-0 bg-primary gap-4 text-tertiary flex flex-col px-4 py-8 justify-start items-center shadow-lg h-screen w-full">
             <h1 className="text-4xl font-bold">
                 <span className="text-secondary">Vehi</span>Track
             </h1>
 
-            <nav className="text-xl font-medium space-y-4">
-                <Link href={route("dashboard")} className={`${route().current("dashboard")&&"text-secondary"} flex h-fit justify-between items-center gap-1  `}>
-                    <Icon className="text-2xl"  icon={"material-symbols:dashboard-rounded"} />
+            <div className="text-xl font-medium space-y-4">
+                <Link
+                    href={route("dashboard")}
+                    className={`${
+                        route().current("dashboard") && "text-secondary"
+                    } flex h-fit justify-between items-center gap-1  `}
+                >
+                    <Icon
+                        className="text-2xl"
+                        icon={"material-symbols:dashboard-rounded"}
+                    />
                     <p>Dashboard</p>
                 </Link>
-                <Link href={route("dashboard")} className="flex h-fit items-center gap-1  ">
-                    <Icon className="text-2xl"  icon={"fluent:vehicle-truck-profile-16-regular"} />
+                <Link
+                    href={route("vehicle.index")}
+                    className={`${
+                        route().current("vehicle*") && "text-secondary"
+                    } flex h-fit items-center gap-1  `}
+                >
+                    <Icon
+                        className="text-2xl"
+                        icon={"fluent:vehicle-truck-profile-16-regular"}
+                    />
                     <p>Kendaraan</p>
                 </Link>
-                <Link href={route("dashboard")} className="flex h-fit items-center gap-1  ">
-                    <Icon className="text-2xl"  icon={"mdi:drivers-license"} />
-                    <p>Driver</p>
+                <Link
+                    href={route("driver.index")}
+                    className={`${
+                        route().current("driver*") && "text-secondary"
+                    } flex h-fit items-center gap-1  `}
+                >
+                    <Icon className="text-2xl" icon={"mdi:drivers-license"} />
+                    <p>Supir</p>
                 </Link>
-                <Link href={route("dashboard")} className="flex h-fit items-center gap-1  ">
-                    <Icon className="text-2xl"  icon={"lets-icons:order-fill"} />
+                <Link
+                    href={route("dashboard")}
+                    className="flex h-fit items-center gap-1  "
+                >
+                    <Icon className="text-2xl" icon={"lets-icons:order-fill"} />
                     <p>Booking</p>
                 </Link>
-                <Link href={route("dashboard")} className="flex h-fit items-center gap-1  ">
-                    <Icon className="text-2xl"  icon={"mdi:fuel"} />
+                <Link
+                    href={route("dashboard")}
+                    className="flex h-fit items-center gap-1  "
+                >
+                    <Icon className="text-2xl" icon={"mdi:fuel"} />
                     <p>Fuel</p>
                 </Link>
-            
-            </nav>
+            </div>
         </div>
     );
 };
