3ebfab9bd92d32ae62fc8a84695cc9bdfdc77c9e
anantariskys <gamersindo555@gmail.com>
2025-01-01 14:55:33 +0700

    feat(model) : add booking model

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\UserActivity;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -35,17 +36,14 @@ class RegisteredUserController extends Controller
             'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
-
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-
         event(new Registered($user));
-
         Auth::login($user);
-
+        UserActivity::logActivity(Auth::user(), 'Register');
         return redirect(route('dashboard', absolute: false));
     }
 }
---
+++ app/Models/Booking.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+
+    protected $guarded = ['id'];
+    public function driver()
+    {
+        return $this->belongsTo(Driver::class);
+    }
+
+    public function vehicle()
+    {
+        return $this->belongsTo(Vehicle::class);
+    }
+    
+    public function administrator()
+    {
+        return $this->belongsTo(User::class, 'administrator_id');
+    }
+    public function approver()
+    {
+        return $this->belongsToMany(User::class, 'booking_approvers', 'booking_id', 'approver_id');
+    }
+
+    public function mine()
+    {
+        return $this->belongsTo(Mines::class,'mines_id');
+    }
+
+    public function approvers()
+    {
+        return $this->belongsToMany(User::class, 'booking_approvers', 'booking_id', 'approver_id')
+                    ->withPivot('status', 'created_at', 'updated_at')
+                    ->withTimestamps();
+    }   
+
+}
