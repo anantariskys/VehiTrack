ddd5b6978bed0b393102f53c7ae4dcf553cc062b
anantariskys <gamersindo555@gmail.com>
2025-01-01 21:43:29 +0700

    feat : add update & delete function

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Driver;
+use App\Models\UserActivity;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class DriverController extends Controller
@@ -28,6 +30,43 @@ class DriverController extends Controller
 
 
         $vehicle = Driver::create($validated);
-        return redirect()->route('driver.index')->with('success', 'Driver created successfully.');
+        if($vehicle){
+            UserActivity::logActivity(Auth::user(), 'Driver created', $vehicle);
+        }
+        return redirect()->route('driver.index')->with('status', 'success')
+        ->with('message', 'Driver berhasil ditambahkan');
+    }
+
+    public function edit($id)
+    {
+        $driver = Driver::find($id);
+        return Inertia::render('Driver/Edit', [
+            'driver' => $driver
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $driver = Driver::find($id);
+        $driver->update($request->all());
+        UserActivity::logActivity(Auth::user(), 'Driver updated', $driver);
+        return redirect()->route('driver.index')->with('status', 'success')
+        ->with('message', 'Driver berhasil diubah');
+    }
+
+    public function destroy($id)
+    {
+        $driver = Driver::find($id);
+
+        if($driver->status == 'assigned'){
+            return redirect()->route('driver.index')->with('status', 'error')
+            ->with('message', 'Driver masih bertugas, tidak bisa dihapus');
+        }
+        $driver->delete();
+        UserActivity::logActivity(Auth::user(), 'Driver deleted', $driver);
+        return redirect()->route('driver.index')->with('status', 'success')
+        ->with('message', 'Driver berhasil dihapus');
     }
 }
+
+
