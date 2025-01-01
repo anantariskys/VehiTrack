c2d9c6237f20700ed7fa4064844e0e787c890321
anantariskys <gamersindo555@gmail.com>
2025-01-01 21:50:51 +0700

    refactor : delete useless column

--- database/migrations/2024_12_30_162804_vehicle.php
+++ database/migrations/2024_12_30_162804_vehicle.php
@@ -18,9 +18,8 @@ return new class extends Migration
             $table->enum('ownership', ['company', 'rental'])->default('company');
             $table->enum('type', ['freight', 'passenger'])->default('freight');
             $table->enum('status', ['available', 'unavailable', 'maintenance'])->default('available');
-            $table->decimal('fuel_consumption', 10, 2);
+            $table->decimal('fuel_cost', 10, 2);
             $table->date('last_service_date')->nullable();
-            $table->date('next_service_date')->nullable();
             $table->timestamps();
         });
     }
