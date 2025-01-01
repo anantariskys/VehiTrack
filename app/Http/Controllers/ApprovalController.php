fb85bbd8e0dbdee976a307fb914a4795dfd84b85
anantariskys <gamersindo555@gmail.com>
2025-01-01 14:54:33 +0700

    feat(controller) : add approval controller

---
+++ app/Http/Controllers/ApprovalController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Booking;
+use App\Models\BookingAprrover;
+use App\Models\Driver;
+use App\Models\Vehicle;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+class ApprovalController extends Controller
+{
+    public function index()
+    {
+        $user = Auth::user();
+
+        $bookings = Booking::with('driver', 'vehicle', 'approvers', 'administrator', 'mine')
+            ->whereHas('approvers', function ($query) use ($user) {
+                $query->where('approver_id', $user->id);
+            })
+            ->get();
+
+
+
+        return Inertia::render('Approval/Index', [
+            'bookings' => $bookings,
+        ]);
+    }
+
+    public function approving($id, $status)
+    {
+
+        $approval = BookingAprrover::where('booking_id', $id)
+            ->where('approver_id', Auth::user()->id)
+            ->first();
+
+        if ($approval) {
+            $approval->status = $status;
+            $approval->save();
+            $allApproval = BookingAprrover::where('booking_id', $id)->get();
+            $approvedCount = $allApproval->where('status', 'approved')->count();
+            if ($approvedCount === $allApproval->count()) {
+                $booking = Booking::find($approval->booking_id);
+                $booking->status = 'approved';
+                $booking->progress = 'ongoing';
+                $booking->save();
+            }
+
+            if ($status === 'rejected') {
+                $booking = Booking::find($approval->booking_id);
+                $booking->status = 'rejected';
+                $booking->save();
+
+                $driver = Driver::find($booking->driver_id);
+                $driver->status = 'available';
+                $driver->save();
+
+                $vehicle = Vehicle::find($booking->vehicle_id);
+                $vehicle->status = 'available';
+                $vehicle->save();
+            }
+            $message = $status === 'approved'
+                ? 'Anda berhasil menyetujui pemesanan!'
+                : 'Anda berhasil menolak pemesanan!';
+            return redirect()->route('approval.index')
+                ->with('status', 'success')
+                ->with('message', $message);
+        }
+        return redirect()->route('approval.index')
+            ->with('status', 'error')
+            ->with('message', 'Approval tidak ditemukan!');
+    }
+}
