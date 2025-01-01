b183d5e415524c9e715fb7783042952ad169ae4e
anantariskys <gamersindo555@gmail.com>
2025-01-01 21:48:07 +0700

    feat(Vehicle) : add relation to table service

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Vehicle extends Model
 {
     protected $guarded = ['id'];
+
+    public function service()
+    {
+        return $this->hasMany(Service::class);
+    }
 }
