896491571967e0ace77eff1d8b20b19cfa17b229
anantariskys <gamersindo555@gmail.com>
2025-01-01 21:44:27 +0700

    feat : add VehicleController

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserActivity;
 use App\Models\Vehicle;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class VehicleController extends Controller
@@ -25,16 +27,52 @@ class VehicleController extends Controller
             'number_plate' => 'required|string|max:255|unique:vehicles,number_plate',
             'ownership' => 'required|in:company,rental',
             'type' => 'required|in:freight,passenger',
-            'status' => 'required|in:available,unavailable,maintenance',
-            'fuel_consumption' => 'required|numeric|min:0',
-            'last_service_date' => 'nullable|date',
-            'next_service_date' => 'nullable|date|after_or_equal:last_service_date',
+            'fuel_cost' => 'required|numeric|min:0',
+
         ]);
 
 
         $vehicle = Vehicle::create($validated);
 
+        if($vehicle){
+            UserActivity::logActivity(Auth::user(), 'Vehicle created', $vehicle);
+        }
+        return redirect()->route('vehicle.index')->with('success', 'Berhasil menambahkan kendaraan');
+    }
 
-        return redirect()->route('vehicle.index')->with('success', 'Vehicle created successfully.');
+    public function edit($id)
+    {
+        $vehicle = Vehicle::find($id);
+        return Inertia::render('Vehicle/Edit', [
+            'vehicle' => $vehicle
+        ]);
+    }
+    public function update(Request $request, $id)
+    {
+        $vehicle = Vehicle::find($id);
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'number_plate' => 'required|string|max:255',
+            'ownership' => 'required|in:company,rental',
+            'type' => 'required|in:freight,passenger',
+            'fuel_cost' => 'required|numeric|min:0',
+        ]);
+
+        $vehicle->update($request->all());
+        UserActivity::logActivity(Auth::user(), 'Vehicle updated', $vehicle);
+        return redirect()->route('vehicle.index')->with('status', 'success')->with('message', 'Kendaraan berhasil diubah');
+    }
+    public function destroy($id)
+    {
+        $vehicle = Vehicle::find($id);
+        if($vehicle->status === 'unavailable'){
+            return redirect()->route('vehicle.index')->with('status', 'error')->with('message', 'Kendaraan sedang tidak tersedia, tidak bisa dihapus');
+        }
+        if($vehicle->status === 'maintenance'){
+            return redirect()->route('vehicle.index')->with('status', 'error')->with('message', 'Kendaraan sedang dalam perbaikan, tidak bisa dihapus');
+        }
+        $vehicle->delete();
+        UserActivity::logActivity(Auth::user(), 'Vehicle deleted', $vehicle);
+        return redirect()->route('vehicle.index')->with('status', 'success')->with('message', 'Kendaraan berhasil dihapus');
     }
 }
