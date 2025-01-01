796cdca3d4a165fd0c98bf1ac728364b93fdaa6c
anantariskys <gamersindo555@gmail.com>
2025-01-01 14:56:45 +0700

    feat(types) : add few types

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -29,3 +29,109 @@ export interface Vehicle {
     updated_at: string; 
 }
   
+
+
+interface Administrator {
+  id: number;
+  name: string;
+  email: string;
+  email_verified_at: string | null;
+  role: string;
+  [key: string]: any;
+}
+
+interface Driver {
+  id: number;
+  name: string;
+  status: string;
+  created_at: string;
+  updated_at: string;
+}
+
+interface Vehicle {
+  id: number;
+  name: string;
+  number_plate: string;
+  ownership: string;
+  type: string;
+  [key: string]: any; 
+}
+
+interface Approver {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+  [key: string]: any; // Untuk properti tambahan jika ada
+}
+
+interface Driver {
+    id: number;
+    name: string;
+    status: string;
+    created_at: string;
+    updated_at: string;
+  }
+
+  interface Approver {
+    id: number;
+    name: string;
+    email: string;
+    role: string;
+    status : string;
+    [key: string]: any;
+  }
+
+  interface Mine{
+    id: number;
+    name: string;
+    region: string;
+    [key: string]: any;
+  }
+
+  interface Service{
+    id: number;
+    name: string;
+    price: number;
+    vehicle_id: number;
+    vehicle : Vehicle
+    status : "ongoing"|"done";
+    description: string;
+    created_at: string;
+    updated_at: string;
+  }
+  interface Log{
+    id: number;
+    user:User;
+    action: string;
+    details : string;
+    created_at: date;
+    updated_at: string;
+  }
+interface Booking {
+  id: number;
+  administrator: User;
+  driver: Driver;
+  vehicle: Vehicle;
+  mine: Mine;
+  approvers: {
+    name:string,
+    position : string,
+    role: string,
+    email :string,
+    pivot:{
+        status:"pending" | "approved" | "rejected"
+    }
+  }[];
+  status: "pending" | "approved" | "rejected";
+  progress : 'done' | 'ongoing' | null;
+  start_date: string;
+  end_date: string;
+  created_at: string;
+  updated_at: string;
+}
+
+interface FlashProps {
+    message: string | null;
+    status: 'success' | 'error' | null;
+}
