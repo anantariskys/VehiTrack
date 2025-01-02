49c9b88e70df9c4ddbc60f1a50cc2a1fabecd08b
anantariskys <gamersindo555@gmail.com>
2025-01-02 12:53:22 +0700

    Update Sidebar.tsx

--- resources/js/Components/Sidebar.tsx
+++ resources/js/Components/Sidebar.tsx
@@ -1,64 +1,94 @@
+import { User } from "@/types";
 import { Icon } from "@iconify/react";
 import { Link } from "@inertiajs/react";
+import { FC, useState } from "react";
 
-const Sidebar = () => {
-    return (
-        <div className="max-w-72 sticky top-0 bg-primary gap-4 text-tertiary flex flex-col px-4 py-8 justify-start items-center shadow-lg h-screen w-full">
-            <h1 className="text-4xl font-bold">
-                <span className="text-secondary">Vehi</span>Track
-            </h1>
+const Sidebar: FC<{ auth: { user: User } }> = ({ auth }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-            <div className="text-xl font-medium space-y-4">
-                <Link
-                    href={route("dashboard")}
-                    className={`${
-                        route().current("dashboard") && "text-secondary"
-                    } flex h-fit justify-between items-center gap-1  `}
-                >
-                    <Icon
-                        className="text-2xl"
-                        icon={"material-symbols:dashboard-rounded"}
-                    />
-                    <p>Dashboard</p>
-                </Link>
-                <Link
-                    href={route("vehicle.index")}
-                    className={`${
-                        route().current("vehicle*") && "text-secondary"
-                    } flex h-fit items-center gap-1  `}
-                >
-                    <Icon
-                        className="text-2xl"
-                        icon={"fluent:vehicle-truck-profile-16-regular"}
-                    />
-                    <p>Kendaraan</p>
-                </Link>
-                <Link
-                    href={route("driver.index")}
-                    className={`${
-                        route().current("driver*") && "text-secondary"
-                    } flex h-fit items-center gap-1  `}
-                >
-                    <Icon className="text-2xl" icon={"mdi:drivers-license"} />
-                    <p>Supir</p>
-                </Link>
-                <Link
-                    href={route("dashboard")}
-                    className="flex h-fit items-center gap-1  "
-                >
-                    <Icon className="text-2xl" icon={"lets-icons:order-fill"} />
-                    <p>Booking</p>
-                </Link>
-                <Link
-                    href={route("dashboard")}
-                    className="flex h-fit items-center gap-1  "
-                >
-                    <Icon className="text-2xl" icon={"mdi:fuel"} />
-                    <p>Fuel</p>
-                </Link>
-            </div>
+  const links = [
+    {
+      href: "dashboard",
+      icon: "material-symbols:dashboard-rounded",
+      label: "Dashboard",
+    },
+    {
+      href: "vehicle.index",
+      icon: "mdi:truck",
+      label: "Kendaraan",
+    },
+    {
+      href: "driver.index",
+      icon: "mdi:drivers-license",
+      label: "Supir",
+    },
+    {
+      href: "service.index",
+      icon: "mdi:tools",
+      label: "Perbaikan Kendaraan",
+    },
+    {
+      href: "usage.index",
+      icon: "mdi:clipboard-text-history",
+      label: "Riwayat Pemakaian",
+    },
+    {
+      href: "log.index",
+      icon: "icon-park-outline:log",
+      label: "Log Aktivitas",
+    },
+  ];
+
+  if (auth.user.role === "administrator") {
+    links.push({
+      href: "booking.index",
+      icon: "mdi:bookmark-border",
+      label: "Pemesanan Kendaraan",
+    });
+  }
+
+  if (auth.user.role === "approver") {
+    links.push({
+      href: "approval.index",
+      icon: "material-symbols:approval-outline",
+      label: "Penyetujuan Pemesanan",
+    });
+  }
+
+  return (
+    <div>
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="lg:hidden fixed top-4 right-4 z-10 p-2 text-white bg-primary rounded-md"
+      >
+        <Icon icon="material-symbols:menu" className="text-2xl" />
+      </button>
+
+      <div
+        className={`lg:max-w-72 lg:w-72 lg:sticky fixed top-0 right-0 bg-primary gap-4 text-tertiary flex flex-col px-4 py-8 justify-start items-center shadow-lg h-screen w-full transform transition-transform duration-300 ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        } lg:translate-x-0`}
+      >
+        <h1 className="text-4xl font-bold">
+          <span className="text-secondary">Vehi</span>Track
+        </h1>
+        <div className=" font-medium space-y-4">
+          {links.map(({ href, icon, label }, index) => (
+            <Link
+              key={index}
+              href={route(href)}
+              className={`${
+                route().current(`${href.split(".")[0]}*`) && "text-secondary"
+              } flex h-fit items-center gap-1`}
+            >
+              <Icon className="text-2xl" icon={icon} />
+              <p>{label}</p>
+            </Link>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
