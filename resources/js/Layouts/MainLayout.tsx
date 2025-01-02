3789a5859af974dda14a14b71d57f8322508c996
anantariskys <gamersindo555@gmail.com>
2025-01-02 12:53:32 +0700

    Update MainLayout.tsx

--- resources/js/Layouts/MainLayout.tsx
+++ resources/js/Layouts/MainLayout.tsx
@@ -1,29 +1,46 @@
 import Dropdown from "@/Components/Dropdown";
 import Sidebar from "@/Components/Sidebar";
-import { User } from "@/types";
+import { FlashProps, User } from "@/types";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { usePage } from "@inertiajs/react";
 import { FC, ReactNode } from "react";
+import Toast from "@/Components/Toast";
+
+import { useState, useEffect } from "react";
+import Modal from "@/Components/Modal";
+// ...
 
 const MainLayout: FC<{
     auth: { user: User };
     children: ReactNode;
     heading: string;
 }> = ({ auth, children, heading }) => {
+    const { flash } = usePage().props as unknown as { flash: FlashProps };
+    const [toast, setToast] = useState<{ message: string; status: "success" | "error" | null } | null>(null);
+
+    useEffect(() => {
+        if (flash.message) {
+            setToast({ message: flash.message, status: flash.status });
+        }
+    }, [flash]);
+
     return (
         <div className="min-h-screen bg-gray-100 flex">
-            <Sidebar />
-            <main className="w-full p-4 space-y-4">
-                <header className="flex bg-secondary shadow-lg p-4 rounded-md justify-between items-center">
-                    <h1 className="text-3xl font-semibold text-tertiary">
+            <Sidebar auth={auth} />
+            <main className="w-full overflow-hidden  lg:p-4 p-4 space-y-2 lg:space-y-4">
+                <header className="flex bg-secondary shadow-lg p-2 lg:p-4 rounded-md justify-between items-center">
+                    <h1 className="md:text-xl text-base lg:text-3xl font-semibold text-tertiary">
                         {heading}
                     </h1>
-                    <Dropdown>
+                    <div className="lg:block hidden">
+
+                    <Dropdown >
                         <Dropdown.Trigger>
                             <span className="inline-flex rounded-md">
                                 <button
                                     type="button"
                                     className="inline-flex items-center rounded-md border border-transparent bg-primary px-3 py-2 text-sm gap-2 font-medium leading-4 transition duration-150 ease-in-out text-tertiary focus:outline-none"
-                                >
+                                    >
                                     <Icon
                                         icon="mdi:account-circle"
                                         className="text-2xl"
@@ -37,12 +54,12 @@ const MainLayout: FC<{
                                         xmlns="http://www.w3.org/2000/svg"
                                         viewBox="0 0 20 20"
                                         fill="currentColor"
-                                    >
+                                        >
                                         <path
                                             fillRule="evenodd"
                                             d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
                                             clipRule="evenodd"
-                                        />
+                                            />
                                     </svg>
                                 </button>
                             </span>
@@ -52,12 +69,21 @@ const MainLayout: FC<{
                                 href={route("logout")}
                                 method="post"
                                 as="button"
-                            >
+                                >
                                 Log Out
                             </Dropdown.Link>
                         </Dropdown.Content>
                     </Dropdown>
+                </div>
                 </header>
+                {toast && (
+                    <Toast
+                    message={toast.message}
+                    status={toast.status}
+                    onClose={() => setToast(null)}
+                    />
+                )}
+                <Modal/>
                 {children}
             </main>
         </div>
@@ -65,3 +91,4 @@ const MainLayout: FC<{
 };
 
 export default MainLayout;
+
