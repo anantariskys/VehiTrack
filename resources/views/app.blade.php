6a0abd40f1f9fd5d11c4313d46673419cdd4c924
anantariskys <gamersindo555@gmail.com>
2025-01-02 12:54:29 +0700

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,17 +6,16 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
+        <link href="https://fonts.bunny.net/css?family=poppins:400,500,600,700&display=swap" rel="stylesheet" />
+
         @routes
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class=" antialiased font-poppins">
         @inertia
     </body>
 </html>
